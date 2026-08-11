73a7c899cbaa3490117a7a508dac80db9b85e9a2
Harshal Pujari <harshalpujari01@gmail.com>
2026-08-11 19:16:48 +0530

    Practiced CSV module and started JSON module

---
+++ readAndWriteData.py
@@ -0,0 +1,15 @@
+import csv
+
+with open("employees.csv", "r") as input_file, open("output.csv", "w", newline="") as output_file:
+
+    reader = csv.reader(input_file)
+    writer = csv.writer(output_file)
+
+    next(reader)
+
+    writer.writerow(["name", "department", "salary"])
+
+    for row in reader:
+        name, dept, sal = row 
+        if dept == "Data" and int(sal)>70000:
+            writer.writerow(row)
---
+++ readJson.py
@@ -0,0 +1,4 @@
+import json, csv
+# with open("employee.json", "r") as file:
+help(json.load) 
+# print(dir(json)) 
