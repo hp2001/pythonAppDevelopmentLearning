53e6b4d9ffaf6121ec5ff061cad771a1837cbc35
Harshal Pujari <harshalpujari01@gmail.com>
2026-08-22 19:54:15 +0530

    learned argparse and created first script

--- 12_moveUsingArgument.py
+++ 12_moveUsingArgument.py
@@ -7,4 +7,8 @@ print(f"Input: {input_file}\nOutput: {output_file}")
 
 import argparse
 parser = argparse.ArgumentParser()
-print(parser)
+parser.add_argument("--input", help="Input file path", required=True)
+parser.add_argument("--min-salary", help="Minimum salary filter", type=int, required=False)
+parser.add_argument("--output", help="Output file path", required=True)
+args = parser.parse_args()
+print(f"Input: {args.input}\nMinimum Salary: {args.min_salary}\nOutput: {args.output}")
---
+++ 13_process.py
@@ -0,0 +1,23 @@
+import argparse, csv
+from pathlib import Path
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--input", help="Input file path", required=True)
+parser.add_argument("--output", help="Output file path", required=True)
+args = parser.parse_args()
+input_file = Path(args.input)
+output_file = Path(args.output)
+
+if input_file.exists():
+    with open(input_file, 'r') as input_f, open(output_file, 'w') as output_f:
+        reader = csv.reader(input_f)
+        writer = csv.writer(output_f)
+        next(reader)  # Skip header
+        writer.writerow(["name", "department", "salary"])
+        for row in reader:
+            name, dept, sal = row
+            if dept == 'Data' and int(sal) > 70000:
+                writer.writerow(row)
+else:
+    print(f"Error: The file {input_file} does not exist.")
+    raise SystemExit(1)
